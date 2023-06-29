0f95fe78bc59396d7474ce547ee033f619e5e03d
ahristov91 <ahristov91@abv.bg>
2023-06-29 15:59:56 +0300

    fix correct sum calculation

--- groceries-shop/src/App.jsx
+++ groceries-shop/src/App.jsx
@@ -24,6 +24,8 @@ export default function App() {
     });
   }
 
+  
+
   function deleteProduct(id) {
     setProducts((currentProducts) => {
       return currentProducts.filter((product) => product.id !== id);
@@ -31,58 +33,28 @@ export default function App() {
   }
 
   const prices = {
-    apple: 3,
-    banana: 5,
-    carrot: 7,
-    tomato: 9,
-    cucumber: 11,
-    orange: 13,
+    apple: 300,
+    banana: 550,
+    carrot: 700,
+    tomato: 930,
+    cucumber: 1100,
+    orange: 1300,
   };
 
-  function calculateTotalPrice(items) {
-    const countMap = {};
-    const dealItems = ["banana", "orange", "tomato"]; // Списък с продуктите, които включват 2 за 3 отстъпка
-    let totalPrice = 0;
-
-    // Изчисляване на броят на всяка добавена позиция
-    for (const item of items) {
-      countMap[item] = (countMap[item] || 0) + 1;
-    }
-
-    // Изчисляване на общата цена, като се приложи отстъпката 2 за 3
-    for (const item of items) {
-      if (item in prices) {
-        const price = prices[item];
-        if (dealItems.includes(item) && countMap[item] >= 3) {
-          countMap[item] -= 2; // Само първите 2 от тези продукти се вземат предвид
-        } else {
-          countMap[item]--; // Намаляване на броя за всички останали продукти
-        }
-        if (countMap[item] > 0) {
-          totalPrice += price;
-        }
-      }
-    }
-
-    return totalPrice;
-  }
-
-  const [totalPrice, setTotalPrice] = useState(() => calculateTotalPrice(products));
-
- 
+  const totalPrice = products.reduce((total, product) => {
+    const price = prices[product.title.toLowerCase()];
+    return total + price;
+  }, 0);
 
   const aws = Math.floor(totalPrice / 100);
   const c = totalPrice % 100;
-  useEffect(() => {
-    const totalPrice = calculateTotalPrice(products);
-    setTotalPrice(totalPrice);
-  }, [products]);
+
   return (
     <>
       <AddProductForm onSubmit={addProduct} />
       <h1 className="header">Added Products</h1>
       <ProductList products={products} deleteProduct={deleteProduct} prices={prices} />
-      <p>Total Price: {totalPrice} c</p>
+      <p>Total Price: {aws} aws {c} c</p>
     </>
   );
 }
